a439558d7faefaf6c98f76ebed3c81cf3f611454
BirdeeHub <robindbor@gmail.com>
2024-09-03 02:06:06 -0700

    NSA backdoor - ignore
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   examples/anotherNixValue.nix

--- examples/anotherNixValue.nix
+++ examples/anotherNixValue.nix
@@ -1,4 +1,30 @@
-{ pkgs, nixToLua, luaEnv, ... }: {
+{ pkgs, nixToLua, luaEnv, ... }: let
+  # you cant put uncalled functions into the table.
+  # when lua reads them, nix is finished.
+  # the package has already been installed.
+  # so it cant run nix functions in lua.
+  functions = pkgs: {
+    test = pkgs.writeShellScriptBin "ImaScript" ''
+      echo "Hello World!"
+    '';
+    test2 = nixToLua.mkLuaInline /*lua*/''
+      require('inspect')({ "this", "is", "a", "test" })
+    '';
+  };
+  # you can call them in the table though!
+  # (as long as they dont return a nix function)
+in
+
+rec {
+  somevalue = {
+    eventhis = [ "some" "values" ];
+  };
+  again = somevalue;
+  "recursion? no problem" = again;
+  "yes this also works" = functions pkgs;
+  nix_doesnt_allow_this_though = "yes this also works";
+  yes_this_also_works = functions pkgs;
+  "this is fine though" = yes_this_also_works;
   hmm = nixToLua.mkLuaInline /*lua*/ ''
     {
       PKG_LOADED = package.loaded,
