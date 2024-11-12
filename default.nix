cbef7711a18b1a6d988d26f56d09440989030eec
birdee <birdee@birdee.com>
2024-11-12 08:13:00 -0800

    Herpderp, shoulda check if it does really compile.
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   default.nix

--- default.nix
+++ default.nix
@@ -66,8 +66,9 @@ with builtins; rec {
       else if value == null then "nil"
       else if isFloat value || isInt value then toString value
       else if isList value then "${luaListPrinter level value}"
-      else if isDerivation value then luaEnclose "${value}"
       else if isLuaInline value then replacer (luaToString value)
+      else if isAttrs value && value ? outPath then luaEnclose "${value.outPath}"
+      else if isDerivation value then luaEnclose "${value}"
       else if isAttrs value then "${luaTablePrinter level value}"
       else replacer (luaEnclose (toString value));
 
