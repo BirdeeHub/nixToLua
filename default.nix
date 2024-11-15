e6839d6ed06f868d25499cffd72e837922b8fbd0
birdee <birdee@birdee.com>
2024-11-14 18:19:28 -0800

    Just committing so I can go home
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   default.nix

--- default.nix
+++ default.nix
@@ -27,8 +27,8 @@ with builtins; let
     types = mapAttrs (n: p: p // { name = n; mk = mkmk n p (p.default or (o: o)); }) proto;
     default_subtype = let
       defvals = attrNames (filterAttrs (n: x: isFunction (x.default or false)) proto);
-      valdef = if defvals == [] then throw "no default type specified"
-        else if length defvals  == 1 then head defvals
+      valdef = if length defvals  == 1 then head defvals
+        else if defvals == [] then throw "no default type specified"
         else throw "multiple default types specified";
     in valdef;
     member = v: (v.__type or null == id && v ? expr) && null != types."${v.expr.type or default_subtype}".name or null;
