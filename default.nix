8ffb685d9e559221ce9c8c2bea2da07a0eab5340
birdee <birdee@birdee.com>
2024-11-14 17:45:15 -0800

    Make that it works in 90% of the cases.  3:30.
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   default.nix

--- default.nix
+++ default.nix
@@ -41,9 +41,9 @@ with builtins; let
   in { inherit types typeof member resolve mkBaseT id default_subtype; };
 
   LIproto = let
-    fixargs = LI: if all (v: isString v || builtins.match ''^([A-Za-z_][A-Za-z0-9_]*|\.\.\.)$'' v != null) (LI.expr.args or [])
-      then concatStringsSep ", " (LI.expr.args or [])
-      else throw "args must be valid lua identifiers";
+    fixargs = LI: if any (v: ! isString v || builtins.match ''^([A-Za-z_][A-Za-z0-9_]*|\.\.\.)$'' v == null) (LI.expr.args or [])
+      then throw "args must be valid lua identifiers"
+      else concatStringsSep ", " (LI.expr.args or []);
   in {
     inline-safe = {
       default = (v: if v ? body then v else { body = v; });
